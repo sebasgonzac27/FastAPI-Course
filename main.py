9964b194a44c8662f8d4fa27961cf2de02d1c28b
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-01-06 16:55:15 -0500

    Refactor movie endpoints and use database ORM

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from starlette.requests import Request
 from jwt_manager import create_token, validate_token
 
 from config.database import Session, engine, Base
-from models.movie import Movie
+from models.movie import Movie as MovieModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 app.title = "Curso de FastAPI"
@@ -148,13 +149,16 @@ def login(user: User = Body(...)):
 
 @app.get('/movies', tags=['Movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())] )
 def get_movies() -> List[Movie]:
-    return JSONResponse(content=movies, status_code=200)
+    db = Session()
+    movies = db.query(MovieModel).all()
+    return JSONResponse(content=jsonable_encoder(movies), status_code=200)
 
 @app.get('/movies/{id}', tags=['Movies'], response_model=Movie,   status_code=200)
 def get_movie(id: int = Path(ge=1, le=100)) -> Movie:
-    for movie in movies:
-        if movie['id'] == id:
-            return JSONResponse(content=movie, status_code=200)
+    db = Session()
+    movie = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if movie:
+      return JSONResponse(content=jsonable_encoder(movie), status_code=200)
     return JSONResponse(content={'error': 'Movie not found'}, status_code=404)
 
 @app.get('/movies/', tags=['Movies'], response_model=List[Movie], status_code=200)
@@ -164,7 +168,10 @@ def get_movie_by_category(category: str = Query(min_length=3, max_length=15)) ->
 
 @app.post('/movies', tags=['Movies'], response_model=dict, status_code=201)
 def create_movie(movie: Movie = Body()) -> dict:
-    movies.append(movie)
+    db = Session()
+    new_movie = MovieModel(**movie.dict())
+    db.add(new_movie)
+    db.commit()
     return JSONResponse(content={"message": "Movie created"}, status_code=201)
 
 @app.put('/movies/{id}', tags=['Movies'], response_model=dict, status_code=200)
